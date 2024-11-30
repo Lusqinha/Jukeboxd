5253b1b42b54c9b75f96647a4ad1436ef6d18b70
Lucas Borges da Silva <lucas18sborges@gmail.com>
2024-11-30 14:19:36 -0300

    API: Definição dos endpoints da api interna comunicando com o banco

---
+++ src/app/api/review/route.ts
@@ -0,0 +1,47 @@
+import prisma from "@/lib/db";
+
+export async function GET(): Promise<Response> { 
+    const reviews = await prisma.review.findMany({
+        include: {
+            user: true
+        }
+    });
+
+    return Response.json({
+        reviews
+    })
+}
+
+export async function POST( req: Request): Promise<Response> {
+    
+    const { userId, spotifyid, rating, text } = await req.json();
+
+    const review = await prisma.review.create({
+        data: {
+
+        userId,
+        spotifyId: spotifyid,
+        rating,
+        text
+
+    }
+    });
+
+    return Response.json({
+        review
+    })
+}
+
+export async function DELETE(req: Request): Promise<Response> {
+    const { id } = await req.json();
+
+    const review = await prisma.review.delete({
+        where: {
+            id
+        }
+    });
+
+    return Response.json({
+        review
+    })
+}
